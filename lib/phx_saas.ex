a5919379b09048d9b7a09624602709dc6a23f34d
sai <3030159@qq.com>
2017-11-24 23:26:57 +0800

    init project

---
+++ lib/phx_saas.ex
@@ -0,0 +1,18 @@
+defmodule PhxSaas do
+  @moduledoc """
+  Documentation for PhxSaas.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PhxSaas.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/phx_saas_test.exs
@@ -0,0 +1,8 @@
+defmodule PhxSaasTest do
+  use ExUnit.Case
+  doctest PhxSaas
+
+  test "greets the world" do
+    assert PhxSaas.hello() == :world
+  end
+end
